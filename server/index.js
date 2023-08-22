ba94560b82bdfa6f5dc09a4fd998937fa8a9045f
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-08-22 18:19:54 +0300

    no cors

--- server/index.js
+++ server/index.js
@@ -29,13 +29,13 @@ app.use(express.json());
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors(
+/*app.use(cors(
   {
     origin: "https://social-media-app-front.vercel.app",
     methods: "GET, HEAD, OPTIONS, PATCH, PUT, DELETE, POST",
     credentials: true
   }
-));
+));*/
 
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
