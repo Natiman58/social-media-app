8a7a3290b637610acda512dd271bf8ed37b35a2a
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-08-23 17:50:50 +0300

    text updates

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Sociopaths!
+          Welcome to My Socials.
         </Typography>
         <Typography style={{marginLeft: "10rem", marginBottom: "1.5rem"}}>
               Dummy Account:- Email: someone@gmail.com and PWD:1234
